2c8316fad2dfae36c747e098a03f0feaf9f1f476
吴肖光 <891211031@qq.com>
2017-10-19 10:29:48 +0800

    拷贝修改上传

--- app/src/main/java/com/example/myandroid3_demo02/MainActivity.java
+++ app/src/main/java/com/example/myandroid3_demo02/MainActivity.java
@@ -1,21 +1,22 @@
 package com.example.myandroid3_demo02;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
-        Log.i("login","---------");
-        Log.i("login","---------");
-        Log.i("login","---------");
-        Log.i("login","---------");
+    }
 
-
+    private void initView() {
+        text = (TextView) findViewById(R.id.text);
     }
 }
