42d98fa144d704faa3280067a27221459f811c36
吴肖光 <891211031@qq.com>
2017-10-19 10:50:24 +0800

    Demo02的第san次测试上传

--- app/src/main/java/com/example/myandroid3_demo02/MainActivity.java
+++ app/src/main/java/com/example/myandroid3_demo02/MainActivity.java
@@ -10,10 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.i("login","---------");//    Log.i("login","---------");    Log.i("login","---------");    Log.i("login","---------");
-        Log.i("login","---------");
-        Log.i("login","---------");
         Log.i("login","---------");
         Log.i("login","---------");
         Log.i("login","---------");
