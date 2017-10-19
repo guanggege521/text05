7dac839a7dd9d958a1953d9037032cb915f83f07
吴肖光 <891211031@qq.com>
2017-10-19 10:36:01 +0800

    Demo02的第san次测试上传

--- app/src/main/java/com/example/myandroid3_demo02/MainActivity.java
+++ app/src/main/java/com/example/myandroid3_demo02/MainActivity.java
@@ -1,22 +1,22 @@
 package com.example.myandroid3_demo02;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
 
-    }
-
-    private void initView() {
-        text = (TextView) findViewById(R.id.text);
+        Log.i("login","---------");//    Log.i("login","---------");    Log.i("login","---------");    Log.i("login","---------");
+        Log.i("login","---------");
+        Log.i("login","---------");
+        Log.i("login","---------");
+        Log.i("login","---------");
+        Log.i("login","---------");
+        Log.i("login","---------");
     }
 }
